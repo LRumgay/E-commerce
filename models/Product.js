8b44e6bd53606687f6065f141b5e6ccb16a9f4d6
Liam Rumgay <Liamrumgay2@gmail.com>
2021-08-20 22:19:20 -0500

    finished product model

--- models/Product.js
+++ models/Product.js
@@ -22,20 +22,20 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      defaultValue: 1
+      isDecimal: true,
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      },
+      isNumeric: true,
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'Category',
         key: 'id',
-        unique: false
+        unique: false,
       }
     }
   },
