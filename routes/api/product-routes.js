24e366cb1d6ad5f85563f3620514660bafee2978
Liam Rumgay <Liamrumgay2@gmail.com>
2021-08-27 22:49:43 -0500

    fixed product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const productData = Product.findAll();
+    const productData = await Product.findAll({
+      include: [{
+        model: Category
+      }, {
+        model: Tag,
+        through: ProductTag
+      }],
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -14,20 +21,12 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get ('/:id', async (req, res) => {
   try {
-    const productData = Product.findOne({
-      where:{
-        id:req.params.id
-      },
+    const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category, through:productTag }]
     });
-
-    if (!productData) {
-      res.status(404).json({ message: 'No location found with this id' });
-      return;
-    }
-
+    
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
